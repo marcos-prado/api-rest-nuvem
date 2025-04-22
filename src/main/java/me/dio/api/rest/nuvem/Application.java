cbbd3f62c903f02c5247d4f1327cd3bba0df9905
Marcos <marcosizaiasprado@gmail.com>
2025-04-22 12:42:40 -0300

    Essa porra não vai

--- src/main/java/me/dio/api/rest/nuvem/Application.java
+++ src/main/java/me/dio/api/rest/nuvem/Application.java
@@ -1,7 +1,9 @@
 package me.dio.api.rest.nuvem;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Táfoda
 
 @SpringBootApplication
 public class Application {
