97d4749662aea29d46d8afa6f88002ab4ad944f8
Marcos <marcosizaiasprado@gmail.com>
2025-04-22 11:22:59 -0300

    start

---
+++ src/main/java/me/dio/api/rest/nuvem/ApiRestNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api.rest.nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/api/rest/nuvem/ApiRestNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.api.rest.nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
